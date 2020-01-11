2a868ed7737dc1802a6b91389fbae8eba50b12a0
gizmore <gizmore@wechall.net>
2020-01-11 22:38:13 +0100

    6.10-r4201_signup_mail_to

--- Module_ActivationAlert.php
+++ Module_ActivationAlert.php
@@ -5,11 +5,28 @@ use GDO\Core\GDO_Module;
 use GDO\User\GDO_User;
 use GDO\Mail\Mail;
 use GDO\Net\GDT_IP;
+use GDO\Mail\GDT_Email;
 
+/**
+ * Sends a mail when a new user is activated.
+ * @author gizmore
+ */
 final class Module_ActivationAlert extends GDO_Module
 {
 	public function onLoadLanguage() { return $this->loadLanguage('lang/activation_alert'); }
 	
+	public function getConfig()
+	{
+		return array(
+			GDT_Email::make('activation_alert_mail_receiver'),
+		);
+	}
+	
+	public function cfgMailReceiver() { return $this->getConfigVar('activation_alert_mail_receiver'); }
+	
+	############
+	### Hook ###
+	############
 	public function hookUserActivated(GDO_User $user)
 	{
 		$this->sendMails($user);
@@ -17,10 +34,26 @@ final class Module_ActivationAlert extends GDO_Module
 	
 	private function sendMails(GDO_User $user)
 	{
-		foreach (GDO_User::admins() as $admin)
+		if ($to = $this->cfgMailReceiver())
 		{
-			$this->sendMail($admin, $user);
+			$this->sendSingleMail($to, $user);
 		}
+		else
+		{
+			foreach (GDO_User::admins() as $admin)
+			{
+				$this->sendMail($admin, $user);
+			}
+		}
+	}
+	
+	private function sendSingleMail($to, GDO_User $user)
+	{
+		$fakeUser = GDO_User::blank(array(
+			'user_name' => GWF_BOT_NAME,
+			'user_email' => $to,
+		));
+		$this->sendMail($fakeUser, $user);
 	}
 	
 	private function sendMail(GDO_User $admin, GDO_User $user)
